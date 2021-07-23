d560b791390faeaa6fc0395a616583f2de00e0fb
Batman2532 <charjan44@gmail.com>
2021-07-23 22:17:45 +0530

    feat: form validation added

--- src/components/register.js
+++ src/components/register.js
@@ -2,11 +2,37 @@ import { Paper,Grid, Avatar,TextField, Button } from '@material-ui/core';
 import React from 'react';
 import './register.css'
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import {Formik, Field, Form, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
+
 function Register(){
 const paperStyle = {padding:'30px 20px',width:300,margin:'50px auto'}
 const header = {margin:'3px'}
 const avatarStyle = {backgroundColor:'gray'}
-const buttonMargin = {marginTop :'10px', color:'gray', border:'2px solid'} 
+const buttonMargin = {marginTop :'10px', color:'gray', border:'2px solid'}
+const initialValues ={
+    firstName:'',
+    lastName:'',
+    email:'',
+    password:'',
+    conformPassword:''
+} 
+const validationSchema=Yup.object().shape({
+    firstName:Yup.string().min(3,"Too short").required("Required"),
+    lastName:Yup.string().min(3,"Too short").required("Required"),
+    email:Yup.string().email("Enter valid mail address").required("Required"),
+    password:Yup.string().min(8,"Enter minimum 8 char").required("Required"),
+    conformPassword:Yup.string().oneOf([Yup.ref('password')],"password must match")
+})
+const onSubmit=(values,props)=>{
+    console.log(values)
+    console.log(props)
+    setTimeout(()=>{
+        props.resetForm()
+        props.setSubmitting(false)
+    },1000)
+}
+
     return( 
         <Grid>
             <Paper  elevation={20} style={paperStyle}> 
@@ -17,14 +43,18 @@ const buttonMargin = {marginTop :'10px', color:'gray', border:'2px solid'}
                 <h2 style={header}>Employee Payroll App</h2>
                 <h2 style={header}>Register</h2>
             </Grid>
-            <form>
-                <TextField fullWidth label='First Name' placeholder= 'Enter Your first name'/>
-                <TextField fullWidth label='Last Name' placeholder= 'Enter Your last name'/>
-                <TextField fullWidth label='Email' placeholder= 'Enter Your email'/>
-                <TextField fullWidth label='Password' placeholder= 'Enter Your password'/>
-                <TextField fullWidth label='Confom Password' placeholder= 'Enter Your conform password'/>
-                <Button Type='submit' varient='contained' fullWidth style={buttonMargin}>Register</Button>
-            </form>
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+                {(props)=>(
+                    <Form>
+                        <Field as={TextField} fullWidth name="firstName" label='First Name' placeholder= 'Enter Your first name' helperText={<ErrorMessage name="firstName">{ msg => <div style={{ color: 'red' }}>{msg}</div> }</ErrorMessage>}/>
+                        <Field as={TextField} fullWidth name="lastName"label='Last Name' placeholder= 'Enter Your last name' helperText={<ErrorMessage name="lastName">{ msg => <div style={{ color: 'red' }}>{msg}</div> }</ErrorMessage>}/>
+                        <Field as={TextField} fullWidth name="email" label='Email' placeholder= 'Enter Your email' helperText={<ErrorMessage name="email">{ msg => <div style={{ color: 'red' }}>{msg}</div> }</ErrorMessage>}/>
+                        <Field as={TextField} fullWidth name="password" label='Password' type="password" placeholder= 'Enter Your password' helperText={<ErrorMessage name="password">{ msg => <div style={{ color: 'red' }}>{msg}</div> }</ErrorMessage>}/>
+                        <Field as={TextField} fullWidth name="conformPassword" label='Confom Password' type="password" placeholder= 'Enter Your conform password' helperText={<ErrorMessage name="conformPassword">{ msg => <div style={{ color: 'red' }}>{msg}</div> }</ErrorMessage>}/>
+                        <Button Type='submit' varient='contained' disabled={props.isSubmitting} fullWidth style={buttonMargin}>{props.isSubmitting?"Loading...":"Register"}</Button>
+                    </Form>
+                )}
+            </Formik>
             </Paper>
         
         </Grid>
